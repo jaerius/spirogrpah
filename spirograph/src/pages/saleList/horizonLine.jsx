4c4d0cc83053f81e620aa539f7097ebc782bfe2c
GirlsLikeMyY <psr2560@naver.com>
2024-05-08 04:59:51 +0900

    Real 프로젝트 git에서 첫 커밋

---
+++ spirograph/src/pages/saleList/horizonLine.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HorizonLine = ({ text }) => {
+  return (
+    <div
+      style={{
+        // display: "block",
+        width: "100%",
+        textAlign: "center",
+        borderBottom: "1px solid #333",
+        lineHeight: "0.1rem",
+        margin: "0.5rem",
+        overflow: "hidden",
+      }}
+    ></div>
+  );
+};
+
+export default HorizonLine;
